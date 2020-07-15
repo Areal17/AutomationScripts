efcf63e73711e7d090dc44166325ad7f30be6da9
Areal17 <post@media-engineering.com>
2020-07-15 12:30:01 +0200

    change to findAll(ul)

--- fetchCompanies.py
+++ fetchCompanies.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
 import requests
 import bs4 as bs 
-import ssl
 import lxml
 
-theURL = "https://de.wikipedia.org/wiki/Liste_der_börsennotierten_deutschen_Unternehmen"
-theContext = ssl.SSLContext()
-response = requests.get(theURL)
+the_URL = "https://de.wikipedia.org/wiki/Liste_der_börsennotierten_deutschen_Unternehmen"
+response = requests.get(the_URL)
 
-soupObject = bs.BeautifulSoup(response.text,'lxml')
+# mit nur einem ul - Element
+soup_Object = bs.BeautifulSoup(response.text,'lxml')
+# lists = soup_Object.find('ul')
+# list_Rows = []
+
+# for row in lists.findAll('li')[0:]:
+#     list_Rows.append(row.text)
+
+# print(list_Rows)
+
+lists = soup_Object.findAll('ul',{'id':'','class':''}) # lists ist ein sog. ResultSet - Es sollen nur die Ergebniss ohne class oder id berücksichtigt werden
+for list in lists:
+    print(len(list))
+
