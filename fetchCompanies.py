a59e316ecbbf76bca304fe0668d5752d64941f32
Areal17 <post@media-engineering.com>
2020-07-17 14:48:01 +0200

    add fetch_website_at_url() function
    this function fetch lists in websites with given url. In this case the
    script fetch a Wikipedia site.

--- fetchCompanies.py
+++ fetchCompanies.py
@@ -3,20 +3,26 @@ import requests
 import bs4 as bs 
 import lxml
 
-the_URL = "https://de.wikipedia.org/wiki/Liste_der_börsennotierten_deutschen_Unternehmen"
-response = requests.get(the_URL)
+wiki_url = "https://de.wikipedia.org/wiki/Liste_der_börsennotierten_deutschen_Unternehmen"
+# Footer hat auch eine Liste. Diese wird mit ausgelesen - sie hat weder id noch class. 
+# Börsennotiertes Unternehmen
+# Deutscher Finanzmarkt
+# Liste (deutsche Unternehmen)
+# werden überflüssigerweise auch 
 
-# mit nur einem ul - Element
-soup_Object = bs.BeautifulSoup(response.text,'lxml')
-# lists = soup_Object.find('ul')
-# list_Rows = []
+def fetch_website_at_url(the_URL):
+    """ fetch website at given url. The text at the li tags will appen to list_rows. list_rows list is the return value"""
+    response = requests.get(the_URL)
+    soup_Object = bs.BeautifulSoup(response.text,'lxml')
+    list_rows = []
+    lists = soup_Object.findAll('ul',{'id':'','class':''}) # lists ist ein sog. ResultSet - Es sollen nur die Ergebniss ohne class oder id berücksichtigt werden
+    for list in lists:
+        lines = list.findAll('li')
+        for line in lines:
+            list_rows.append(line.text)
+    return list_rows
 
-# for row in lists.findAll('li')[0:]:
-#     list_Rows.append(row.text)
 
-# print(list_Rows)
-
-lists = soup_Object.findAll('ul',{'id':'','class':''}) # lists ist ein sog. ResultSet - Es sollen nur die Ergebniss ohne class oder id berücksichtigt werden
-for list in lists:
-    print(len(list))
-
+companies = fetch_website_at_url(wiki_url)
+for company in companies:
+    print(company) 
